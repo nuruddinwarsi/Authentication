c7cb6a9570981cd4d1a1d5c7b6ec02baf6a73984
nuruddinwarsi <33999767+nuruddinwarsi@users.noreply.github.com>
2019-05-04 15:47:04 +0530

    Updated app.js and user.js

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,12 +1,28 @@
 var express = require("express"),
-    mongoose = require("mongoose");
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    session = require("express-session"),
+    localStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
 
 mongoose.connect("mongodb://localhost:27017/authdemo", {
     useNewUrlParser: true
 });
 
+app.use(session({
+    // Secret is the keyword used to encode and decode the sessions
+    // It can be bassically and keyword or sentence
+    secret: "Kamehameha"
+}));
+
 var app = express();
 app.set("view engine", "ejs");
+
+// Tell express to use required passport package and sets it up
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", function (req, res) {
     res.render("home");
